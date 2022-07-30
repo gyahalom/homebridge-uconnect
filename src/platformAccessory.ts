d9f92041dc6d5c1b592fbc4858c2d39083ab1cd4
Gilad Yahalom <gilad.yahalom@gmail.com>
2022-07-30 14:37:43 -0700

    Update platform classes

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -1,141 +1,150 @@
-import { Service, PlatformAccessory, CharacteristicValue } from 'homebridge';
-
-import { ExampleHomebridgePlatform } from './platform';
+import { PlatformAccessory, CharacteristicValue } from 'homebridge';
+import { UconnectHomebridgePlatform } from './platform';
+import * as uapi from './uconnectApi';
 
 /**
  * Platform Accessory
  * An instance of this class is created for each accessory your platform registers
  * Each accessory may expose multiple services of different service types.
  */
-export class ExamplePlatformAccessory {
-  private service: Service;
-
-  /**
-   * These are just used to create a working example
-   * You should implement your own code to track the state of your accessory
-   */
-  private exampleStates = {
-    On: false,
-    Brightness: 100,
-  };
+export class UconnectPlatformAccessory {
+  private lockState: CharacteristicValue;
+  private unlockState: CharacteristicValue;
 
   constructor(
-    private readonly platform: ExampleHomebridgePlatform,
+    private readonly platform: UconnectHomebridgePlatform,
     private readonly accessory: PlatformAccessory,
   ) {
 
+    this.lockState = this.platform.Characteristic.LockCurrentState.UNKNOWN;
+    this.unlockState = this.platform.Characteristic.LockCurrentState.UNKNOWN;
+
     // set accessory information
     this.accessory.getService(this.platform.Service.AccessoryInformation)!
-      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Default-Manufacturer')
-      .setCharacteristic(this.platform.Characteristic.Model, 'Default-Model')
-      .setCharacteristic(this.platform.Characteristic.SerialNumber, 'Default-Serial');
+      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Manufacturer')
+      .setCharacteristic(this.platform.Characteristic.Model, 'Model')
+      .setCharacteristic(this.platform.Characteristic.SerialNumber, 'Serial');
 
-    // get the LightBulb service if it exists, otherwise create a new LightBulb service
     // you can create multiple services for each accessory
-    this.service = this.accessory.getService(this.platform.Service.Lightbulb) || this.accessory.addService(this.platform.Service.Lightbulb);
+    const lockService = this.accessory.getService('Lock Service') ||
+      this.accessory.addService(this.platform.Service.LockMechanism, 'Lock Service',
+        accessory.context.device.vin + '-lock');
+
+    const unlockService = this.accessory.getService('Unlock Service') ||
+      this.accessory.addService(this.platform.Service.LockMechanism, 'Unlock Service',
+        accessory.context.device.vin + '-unlock');
 
     // set the service name, this is what is displayed as the default name on the Home app
     // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
-    this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.exampleDisplayName);
+    lockService.setCharacteristic(this.platform.Characteristic.Name,
+      accessory.context.vehicle.title + ' Lock');
+    unlockService.setCharacteristic(this.platform.Characteristic.Name,
+      accessory.context.vehicle.title + ' Unlock');
 
     // each service must implement at-minimum the "required characteristics" for the given service type
     // see https://developers.homebridge.io/#/service/Lightbulb
+    lockService.getCharacteristic(this.platform.Characteristic.LockCurrentState)
+      .onGet(this.handleLockCurrentStateGet.bind(this));
+    unlockService.getCharacteristic(this.platform.Characteristic.LockCurrentState)
+      .onGet(this.handleUnlockCurrentStateGet.bind(this));
 
-    // register handlers for the On/Off Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.On)
-      .onSet(this.setOn.bind(this))                // SET - bind to the `setOn` method below
-      .onGet(this.getOn.bind(this));               // GET - bind to the `getOn` method below
+    lockService.getCharacteristic(this.platform.Characteristic.LockTargetState)
+      .onGet(this.handleLockTargetStateGet.bind(this))
+      .onSet(this.handleLockTargetStateSet.bind(this));
 
-    // register handlers for the Brightness Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.Brightness)
-      .onSet(this.setBrightness.bind(this));       // SET - bind to the 'setBrightness` method below
+    unlockService.getCharacteristic(this.platform.Characteristic.LockTargetState)
+      .onGet(this.handleUnlockTargetStateGet.bind(this))
+      .onSet(this.handleUnlockTargetStateSet.bind(this));
 
-    /**
-     * Creating multiple services of the same type.
-     *
-     * To avoid "Cannot add a Service with the same UUID another Service without also defining a unique 'subtype' property." error,
-     * when creating multiple services of the same type, you need to use the following syntax to specify a name and subtype id:
-     * this.accessory.getService('NAME') || this.accessory.addService(this.platform.Service.Lightbulb, 'NAME', 'USER_DEFINED_SUBTYPE_ID');
-     *
-     * The USER_DEFINED_SUBTYPE must be unique to the platform accessory (if you platform exposes multiple accessories, each accessory
-     * can use the same sub type id.)
-     */
-
-    // Example: add two "motion sensor" services to the accessory
-    const motionSensorOneService = this.accessory.getService('Motion Sensor One Name') ||
-      this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor One Name', 'YourUniqueIdentifier-1');
-
-    const motionSensorTwoService = this.accessory.getService('Motion Sensor Two Name') ||
-      this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor Two Name', 'YourUniqueIdentifier-2');
-
-    /**
-     * Updating characteristics values asynchronously.
-     *
-     * Example showing how to update the state of a Characteristic asynchronously instead
-     * of using the `on('get')` handlers.
-     * Here we change update the motion sensor trigger states on and off every 10 seconds
-     * the `updateCharacteristic` method.
-     *
-     */
-    let motionDetected = false;
-    setInterval(() => {
-      // EXAMPLE - inverse the trigger
-      motionDetected = !motionDetected;
-
-      // push the new value to HomeKit
-      motionSensorOneService.updateCharacteristic(this.platform.Characteristic.MotionDetected, motionDetected);
-      motionSensorTwoService.updateCharacteristic(this.platform.Characteristic.MotionDetected, !motionDetected);
-
-      this.platform.log.debug('Triggering motionSensorOneService:', motionDetected);
-      this.platform.log.debug('Triggering motionSensorTwoService:', !motionDetected);
-    }, 10000);
   }
 
   /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, turning on a Light bulb.
+   * Handle requests to get the current value of the "Lock Current State" characteristic
    */
-  async setOn(value: CharacteristicValue) {
-    // implement your own code to turn your device on/off
-    this.exampleStates.On = value as boolean;
+  handleLockCurrentStateGet() {
+    this.platform.log.debug('Triggered GET LockCurrentState');
 
-    this.platform.log.debug('Set Characteristic On ->', value);
+    return this.lockState;
+  }
+
+  handleUnlockCurrentStateGet() {
+    this.platform.log.debug('Triggered GET UnlockCurrentState');
+
+    return this.unlockState;
   }
 
   /**
-   * Handle the "GET" requests from HomeKit
-   * These are sent when HomeKit wants to know the current state of the accessory, for example, checking if a Light bulb is on.
-   *
-   * GET requests should return as fast as possbile. A long delay here will result in
-   * HomeKit being unresponsive and a bad user experience in general.
-   *
-   * If your device takes time to respond you should update the status of your device
-   * asynchronously instead using the `updateCharacteristic` method instead.
-
-   * @example
-   * this.service.updateCharacteristic(this.platform.Characteristic.On, true)
+   * Handle requests to get the current value of the "Lock Target State" characteristic
    */
-  async getOn(): Promise<CharacteristicValue> {
-    // implement your own code to check if the device is on
-    const isOn = this.exampleStates.On;
+  handleLockTargetStateGet() {
+    this.platform.log.debug('Triggered GET LockTargetState');
 
-    this.platform.log.debug('Get Characteristic On ->', isOn);
+    // set this to a valid value for LockTargetState
+    const currentValue = this.platform.Characteristic.LockTargetState.SECURED;
 
-    // if you need to return an error to show the device as "Not Responding" in the Home app:
-    // throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
+    return currentValue;
+  }
 
-    return isOn;
+  handleUnlockTargetStateGet() {
+    this.platform.log.debug('Triggered GET UnlockTargetState');
+
+    // set this to a valid value for LockTargetState
+    const currentValue = this.platform.Characteristic.LockTargetState.UNSECURED;
+
+    return currentValue;
   }
 
   /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, changing the Brightness
+   * Handle requests to set the "Lock Target State" characteristic
    */
-  async setBrightness(value: CharacteristicValue) {
-    // implement your own code to set the brightness
-    this.exampleStates.Brightness = value as number;
+  async handleLockTargetStateSet(value: CharacteristicValue) {
+    this.platform.log.debug('Triggered SET LockTargetState:', value);
+    if (value === this.platform.Characteristic.LockTargetState.SECURED) {
+      if (await uapi.auth(this.platform.username, this.platform.password)) {
+        this.lockState = this.platform.Characteristic.LockTargetState.UNSECURED;
+        const requestId = await uapi.lockCar(this.accessory.context.vehicle.vin, this.platform.pin);
+        // TODO: Check no error in service ID
+        // Wait for service request to complete within timeout
+        const status = await uapi.checkLockStatus(this.accessory.context.vehicle.vin, requestId,
+          this.platform.timeout);
+        this.platform.log.debug('Request status ended with:', status);
+        if (status === 'SUCCESS') {
+          this.lockState = this.platform.Characteristic.LockCurrentState.SECURED;
+          setTimeout(() => {
+            this.lockState = this.platform.Characteristic.LockCurrentState.UNKNOWN;
+          }, 3000);
+        } else {
+          this.lockState = this.platform.Characteristic.LockCurrentState.UNKNOWN;
+        }
+      } else {
+        this.platform.log.warn('Failed to authenticate');
+      }
+    }
+  }
 
-    this.platform.log.debug('Set Characteristic Brightness -> ', value);
+  handleUnlockTargetStateSet(value: CharacteristicValue) {
+    this.platform.log.debug('Triggered SET UnlockTargetState:', value);
+    if (value === this.platform.Characteristic.LockTargetState.UNSECURED) {
+      if (await uapi.auth(this.platform.username, this.platform.password)) {
+        this.lockState = this.platform.Characteristic.LockTargetState.UNSECURED;
+        const requestId = await uapi.unlockCar(this.accessory.context.vehicle.vin, this.platform.pin);
+        // TODO: Check no error in service ID
+        // Wait for service request to complete within timeout
+        const status = await uapi.checkUnlockStatus(this.accessory.context.vehicle.vin, requestId,
+          this.platform.timeout);
+        this.platform.log.debug('Request status ended with:', status);
+        if (status === 'SUCCESS') {
+          this.unlockState = this.platform.Characteristic.LockCurrentState.UNSECURED;
+          setTimeout(() => {
+            this.unlockState = this.platform.Characteristic.LockCurrentState.UNKNOWN;
+          }, 3000);
+        } else {
+          this.unlockState = this.platform.Characteristic.LockCurrentState.UNKNOWN;
+        }
+      } else {
+        this.platform.log.warn('Failed to authenticate');
+      }
+    }
   }
 
 }
